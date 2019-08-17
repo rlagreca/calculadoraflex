42c27a24ecb57f677c011fe1e7765ea69b33e4b1
Logon Aluno <logonaluno@local.com>
2019-08-17 11:07:42 -0300

    commit

---
+++ app/src/main/java/br/com/rlagreca/calculadoraflex/ui/result/ResultActivity.kt
@@ -0,0 +1,13 @@
+package br.com.rlagreca.calculadoraflex.ui.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.rlagreca.calculadoraflex.R
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
---
+++ app/src/main/java/br/com/rlagreca/calculadoraflex/ui/signup/SignupActivity.kt
@@ -0,0 +1,13 @@
+package br.com.rlagreca.calculadoraflex.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.rlagreca.calculadoraflex.R
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
